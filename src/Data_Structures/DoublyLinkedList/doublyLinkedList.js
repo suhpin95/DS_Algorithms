8086a83205f093f4a350cd9286d1d709b3501f2e
Suhas <suhpin95@gmail.com>
2020-08-17 18:34:15 +0530

    Changed the folder structure

---
+++ src/Data_Structures/DoublyLinkedList/doublyLinkedList.js
@@ -0,0 +1,159 @@
+class Node{
+    constructor(data,next = null, prev = null){
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+class DoublyLinkedList {
+    constructor(head = null, tail = null){
+        this.head = head;
+        this.tail = tail;
+        this.length = 0;
+    }
+//     insert the element at the end of the list
+    push(data){
+        let newNode = new Node(data);
+        if(!this.tail){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;     
+        return this;
+    }
+//     delete at the end of the list
+    pop(){
+        if(!this.tail)return null
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }
+        let prevNode = this.tail.prev;
+        this.tail = prevNode;
+
+        let deletedNode = prevNode.next;  
+        this.tail.next = null;
+        
+        deletedNode.prev = null; 
+        this.length--;
+        return deletedNode;
+    }
+// Insert at the head
+    unshift(data){
+        let newNode = new Node(data);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else{
+            // get the first element
+            let firstNode = this.head;
+            // set the new Node as the head
+            firstNode.prev = newNode;
+            newNode.next = firstNode;
+            this.head = newNode
+        }
+        this.length++;
+        return this;
+    }
+// Delete at the head
+    shift(){
+        let deleteNode = this.head;
+        if(this.length === 1) {
+            this.tail = null;
+            this.head = null;
+        }
+        else{
+            this.head = deleteNode.next;
+            deleteNode.next = null; 
+            this.head.prev = null;
+        }
+        this.length --;
+        return deleteNode;
+    }
+    // Get element using the index;
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        let node;
+        // Check from which point this closer
+        let fromStart = index  
+            ,fromEnd = this.length - index;
+        index = Math.min(fromEnd,fromStart);
+        // closer from end
+        if(index === fromEnd){
+            node = this.tail;
+            for(var i = this.length - 1; i > index; i--){
+                node = node.prev;
+            }
+            return node;
+        } else { 
+            node = this.head;
+            for(var i = 0; i < index; i++){
+                node = node.next;
+            }
+            return node;
+        }
+    }
+    // Update element using the index;
+    set(index,val){
+        let foundNode = this.get(index);
+        if(!!foundNode){
+            foundNode.data = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index,val){
+        // if 0 or this.length handle the case
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+
+        var newNode = new Node(val);
+        var beforeNode = this.get(index-1);
+        var afterNode = beforeNode.next;
+        
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
+        this.length++;    
+        return true;
+    }
+    remove(index){
+        if(index < 0 || index >= this.length) return false;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1 ) return this.pop();
+
+        let prevNode = this.get(index - 1);
+        let deletedNode = prevNode.next;
+        prevNode.next = deletedNode.next;
+        deletedNode.next.prev = prevNode;
+
+        deletedNode.next = null, deletedNode.prev = null;
+        this.length--;
+        return deletedNode;
+    }
+    reverse(){
+        let current = this.tail;
+        for(var i = 0; i < this.length; i++){
+            if(current){
+                console.log(current.data);
+                current = current.prev;    
+            }
+            if(!current) return "Reverse Traversal completed"
+        }
+    }
+}
+
+let doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.push(10)
+doublyLinkedList.push(20)
+doublyLinkedList.push(30)
+doublyLinkedList.pop();
+doublyLinkedList.unshift(0);
+doublyLinkedList.shift();
+
+console.log("After all the operations");
+console.log(doublyLinkedList);
---
+++ src/Data_Structures/SinglyLinkedList/singlyLinkedList.js
@@ -0,0 +1,164 @@
+class Node{
+    constructor(data,next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0; 
+    }
+//    Insert at the end
+    push(data){
+        let node = new Node(data);
+        
+        if(!this.tail){
+            this.head = node;
+            this.tail = this.head;
+        } else{
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
+//    Delete at the end
+    pop(){
+        if(!this.head)return undefined;
+
+        let current = this.head;
+        let newTail = current;
+
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+
+       return current;
+    }
+//  Delete at the end
+    shift(){
+       if(!this.head) return undefined;
+       
+       let current = this.head;
+       this.head = current.next;
+       this.length--;
+
+       if(this.length === 0){
+           this.tail = null;
+       }
+       return current;        
+    }
+//  Insert at the end
+    unshift(data){
+        let newNode = new Node(data);
+
+        if(!this.head){
+             this.head = newNode;
+             this.tail = newNode;
+        } else{
+            newNode.next = this.head;
+            this.head = newNode;   
+        }
+        this.length++;
+        return this;
+    }
+// Get the element at a particular index 
+    get(index){
+        if(index >= this.length || index < 0)return null;
+        else{
+            let current = this.head;
+            for(var i = 0; i < index; i++){
+                current = current.next;
+            }
+            return current;
+        }
+    }
+// Update the element at a particular index 
+    set(index,data){
+        let current = this.get(index);
+        if(current){
+           current.data = data;    
+           return true;
+        } else{
+            return false;
+        }
+        
+    }    
+// Insert element at the index
+    insert(index,val){
+        if(index < 0 || index > this.length) return false;
+        
+        if(index === 0 ){
+           return !!this.unshift(val);
+        }
+        if(index === this.length){
+            return !!this.push(val);
+        }
+//         fetch the prev element index
+        let prev = this.get(index-1);
+        let newNode = new Node(val);
+        
+        newNode.next = prev.next;
+        prev.next = newNode;
+
+        this.length++;
+        return true;
+    }
+//     remove at a specific index
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return !!this.shift();
+        if(index === this.length -  1) return !!this.pop();
+
+//      Get Previous Node
+        let prevNode = this.get(index-1);
+        let deletingNode = prevNode.next;
+
+        prevNode.next = deletingNode.next;
+   
+        this.length--;
+        return deletingNode;
+    }
+
+    printUsingArray(){
+        let result = [];
+        let current = this.head
+        for(var i = 0; i < this.length; i++){
+            result.push(current.data)
+            current = current.next;
+        }
+        return result;
+    }
+
+//     Reverse    
+    reverse(){
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      
+      var next;
+      var prev = null;
+      
+      for(var i = 0; i < this.length; i++){
+        next = node.next;
+        node.next = prev;
+       
+        prev = node;
+        node = next;
+      }
+      return this;
+    }
+}
